9876d2929836029c39651d6cfb5bf05c2e80ca4b
SvitlanaDomanova <majba161@gmail.com>
2023-03-29 10:00:34 +0300

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,60 +13,63 @@ const refs = {
     minutes: document.querySelector('[data-minutes]'),
     seconds: document.querySelector('[data-seconds]'),
   };
+  
+
+  refs.startBtn.disabled = true;
+
+  let deltaTime = 0;
+  let timerId = null;
   let selectedDate = null;
+
   const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-
     onClose(selectedDates) {
-     
-      selectedDate = selectedDates[0].getTime();
-     
-      const deltaDate = selectedDate - Date.now();
-      
-      if (deltaDate <= 0) {
+     selectedDate = selectedDates[0];
+     if (selectedDates[0] < new Date()) {
+        window.alert('Вибрана дата в минулому');
+        return
+     } else {
         refs.startBtn.disabled = false;
-        return (
-          Notify.failure('Вибраний час в минулому'),
-          (refs.startBtn.disabled = true)
-        );
-      }
+     }
     },
   };
 
   flatpickr(refs.input, options); 
-  refs.startBtn.addEventListener('click', () => {
-    refs.startBtn.disabled = true;
-    Notify.info('Відлік почато');
 
-    let intervalId = setInterval(() => {
+ const handleBtnClick = () => {
+    timerId = setInterval(() => {
+        deltaTime = selectedDate - new Date();
+        console.log(deltaTime);
+         
+      refs.days.textContent = convertMs(deltaTime).days;
+      refs.hours.textContent = convertMs(deltaTime).hours;
+      refs.minutes.textContent = convertMs(deltaTime).minutes;
+      refs.seconds.textContent = convertMs(deltaTime).seconds;
 
-      const deltaTime = selectedDate - Date.now();
-      if (deltaTime <= 0) {
-        clearInterval(intervalId);
-        return Notify.success('Відлік закінчено');
+      if (deltaTime <= 1000) {
+        clearInterval(timerId);
       }
-      const reverseTimer = convertMs(deltaTime);
-      
-      refs.days.textContent = addLeadingZero(reverseTimer.days);
-      refs.hours.textContent = addLeadingZero(reverseTimer.hours);
-      refs.minutes.textContent = addLeadingZero(reverseTimer.minutes);
-      refs.seconds.textContent = addLeadingZero(reverseTimer.seconds);
     }, 1000);
-  });
-  
+ }
+    refs.startBtn.addEventListener('click', handleBtnClick);
+     
   function convertMs(ms) {
-
+    // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-
+  
+    // Remaining days
     const days = Math.floor(ms / day);
+    // Remaining hours
     const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
     const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   
     return { days, hours, minutes, seconds };
@@ -75,3 +78,4 @@ const refs = {
   function addLeadingZero(value) {
     return String(value).padStart(2, '0');
   }
+  
